bf99d99b547584f6b009985159c3f7c1c4e9c854
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-08-13 09:27:38 +0200

    edit css styles choice difficult Page

--- frontend/src/pages/JSChoicePage.tsx
+++ frontend/src/pages/JSChoicePage.tsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+
 
 const JSChoicePage = () => {
+    const [selectDifficult, setSelectDifficult] = useState('')
 
+    console.log(selectDifficult)
     
 
     return (
         <div className="m-16 items-center flex flex-col gap-8">
-          <h2 className=" font-bold text-2xl">Select the difficulty of the questions</h2>
-          <div className=' w-[50%]  flex justify-around'>
-            <p className='p-4 px-8 cursor-pointer border-green-300 border-2 font-semibold text-2xl text-green-400 hover:bg-green-400 hover:text-white rounded-xl'>Easy</p>
-            <p className='p-4 px-8 cursor-pointer border-orange-500 border-2 font-semibold text-2xl text-orange-500 hover:bg-orange-500 hover:text-white rounded-xl'>Medium</p>
-            <p className='p-4 px-8 cursor-pointer border-red-300 border-2 font-semibold text-2xl text-red-300 hover:bg-red-300 hover:text-white rounded-xl'>Hard</p>
+          <h2 className=" font-bold text-md sm:text-2xl text-center flex-wrap">Select the difficulty of the questions</h2>
+          <div className=' lg:w-[50%] w-[30%] flex-col gap-4 lg:flex-row text-center flex justify-around items-center'>
+            <p onClick={() => setSelectDifficult('easy')} className=' min-w-[8rem] p-2 sm:p-4 px-8 cursor-pointer border-green-300 border-2 font-semibold text-sm sm:text-2xl text-green-400 hover:bg-green-400 hover:text-white rounded-xl'>Easy</p>
+            <p onClick={() => setSelectDifficult('medium')} className=' min-w-[8rem] p-2 sm:p-4 px-8 cursor-pointer border-orange-500 border-2 font-semibold text-sm sm:text-2xl text-orange-500 hover:bg-orange-500 hover:text-white rounded-xl'>Medium</p>
+            <p onClick={() => setSelectDifficult('hard')} className=' min-w-[8rem] p-2 sm:p-4 px-8 cursor-pointer border-red-300 border-2 font-semibold text-sm sm:text-2xl text-red-300 hover:bg-red-300 hover:text-white rounded-xl'>Hard</p>
           </div>
-          <Link
-            to="js_test_page"
-            className=" font-semibold text-2xl border-solid border-2 border-blue-500 p-8 rounded-xl hover:bg-blue-500 hover:text-white"
+          
+            
+            <button
+            
+            className={`my-[20%] ${!selectDifficult && 'hidden'} font-semibold text-md sm:text-2xl border-solid border-2 border-blue-500 p-4 sm:p-8 rounded-xl hover:bg-blue-500 hover:text-white`}
           >
             Start Test
-          </Link>
+          </button>
+          
+          
         </div>
       );
 }
