12b9334265c182cd5f7b28cb5bb7586b4366eb4d
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-08-15 05:44:27 +0200

    edit home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -7,16 +7,31 @@ const HomePage = () => {
     <div className="m-16 items-center flex flex-col gap-8">
       <h2 className=" font-bold text-2xl">What you would like to learn now???</h2>
       <Link
-        to="js_choice_page"
-        className=" font-semibold text-2xl border-solid border-2 border-blue-500 p-8 rounded-xl hover:bg-blue-500 hover:text-white"
+        to="js_test_page"
+        className="min-w-[12rem] text-center font-semibold text-2xl border-solid border-2 border-blue-500 p-8 rounded-xl hover:bg-blue-500 hover:text-white"
       >
         JavaScript
       </Link>
       <Link
-        to="js_test_page"
-        className=" font-semibold text-2xl border-solid border-2 border-blue-500 p-8 rounded-xl hover:bg-blue-500 hover:text-white"
+        to="#"
+        className="min-w-[12rem] text-center font-semibold text-2xl p-8 rounded-xl bg-gray-600 text-white pointer-events-none cursor-default" 
       >
-        JavaScript
+        CSS 
+        <p className="text-sm font-light text-red-400">(in progress)</p>
+      </Link>
+      <Link
+        to="#"
+        className="min-w-[12rem] text-center font-semibold text-2xl p-8 rounded-xl bg-gray-600 text-white pointer-events-none cursor-default" 
+      >
+        Python 
+        <p className="text-sm font-light text-red-400">(in progress)</p>
+      </Link>
+      <Link
+        to="#"
+        className="min-w-[12rem] text-center font-semibold text-2xl p-8 rounded-xl bg-gray-600 text-white pointer-events-none cursor-default" 
+      >
+        React 
+        <p className="text-sm font-light text-red-400">(in progress)</p>
       </Link>
     </div>
   );
